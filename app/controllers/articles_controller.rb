6a86a8b44874d0b8dbb6ab6a435633a49a7bb686
Maulid Rezki <maulidrezki13@gmail.com>
2022-01-25 13:10:06 +0700

    fixed old changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,42 +1,58 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
   def index
     @articles = Article.all
   end
 
+  # GET /articles/1
   def show
-    @articles = params[:id]
-    @article = Article.find(@articles)
   end
 
+  # GET /articles/new
   def new
-    @articles = Article.new
+    @article = Article.new
   end
 
-  def create
-    @article = Article.new(require_article_params)
-    @article.save
-    redirect_to articles_path(@article)
-  end
-
+  # GET /articles/1/edit
   def edit
-    @articles = Article.find(params[:id])
   end
 
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /articles/1
   def update
-    @article = Article.find(params[:id])
-    @article.update(require_article_params)
-    redirect_to articles_path
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
   end
 
+  # DELETE /articles/1
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
-  def require_article_params
-    params.require(:article).permit(:title, :context)
-  end
+    # Only allow a trusted parameter "white list" through.
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+Article.destroy_all
 
-10.times do
-  Article.create([{title: Faker::Marketing.buzzwords, context: Faker::Hipster.sentence(word_count: 3, supplemental: false, random_words_to_add: 4)}])
+def fake_content
+  paragraphs = []
+  paragraphs << Faker::Lorem.paragraph
+  paragraphs << "![](http://lorempixel.com/800/300/city/)"
+  paragraphs << "## #{Faker::Company.catch_phrase}"
+  paragraphs << Faker::Lorem.paragraphs(2)
+  paragraphs << "**#{Faker::Lorem.word}** #{Faker::Lorem.sentences.join}"
+  paragraphs << "## #{Faker::Company.catch_phrase}"
+  paragraphs << "#{Faker::Lorem.sentences.join} [#{Faker::Commerce.product_name}](#{Faker::Internet.url}) #{Faker::Lorem.sentences.join}"
+  paragraphs << Faker::Lorem.paragraphs(3)
+  paragraphs.flatten.join("\n\n")
+end
+
+20.times do
+  Article.create(
+    title: Faker::Company.catch_phrase,
+    content: fake_content
+  )
 end
